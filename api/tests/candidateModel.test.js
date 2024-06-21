a37edf3a7966efdc17c780570640f99182137056
ahmed kadry <ahmedkadry123a@gmail.com>
2024-06-21 12:09:48 +0200

    testing

--- api/tests/candidateModel.test.js
+++ api/tests/candidateModel.test.js
@@ -6,22 +6,23 @@ describe('Candidate class', () => {
     const email = 'test';
     const first_name = 'test';
     const last_name = 'test';
-    const phone_number = '12'
     const time_interval = '1';
     const linkedin = 'test';
     const github = 'test';
     const text = 'Test';
+    const phone_number = '12'
 
     const candidate = new Candidate(email, first_name, last_name, time_interval, linkedin, github, text);
 
     expect(candidate.email).toBe(email);
     expect(candidate.first_name).toBe(first_name);
     expect(candidate.last_name).toBe(last_name);
-    expect(candidate.phone_number).toBe(phone_number);
     expect(candidate.time_interval).toBe(time_interval);
     expect(candidate.linkedin).toBe(linkedin);
     expect(candidate.github).toBe(github);
-    expect(candidate.text).toBe(text);
+    expect(candidate.text).toBe(text);    
+    expect(candidate.phone_number).toBe(phone_number);
+
   });
 
 });
